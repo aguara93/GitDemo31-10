78b4d24fddd1a0dc229c5446ac0bd4afd784a58c
aguara93 <ag.daleki@gmail.com>
2024-10-31 10:04:45 +0100

    Add project files.

---
+++ GitDemo31-10/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitDemo31_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
